41e394ef205d4c80674049b43c590b223d7f1e78
koda0001 <wiktor.wacalw@dave-it.pl>
2026-04-03 18:18:07 +0200

    fix 3 for db

--- getmate/prisma/prisma.config.ts
+++ getmate/prisma/prisma.config.ts
@@ -1,7 +1,16 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const adapter = process.env.DATABASE_URL;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db = new PrismaClient({
-  adapter,
-});
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.DATABASE_URL !== "production") globalForPrisma.prisma = prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// const adapter = process.env.DATABASE_URL;
+
+// export const db = new PrismaClient({
+//   adapter,
+// });
