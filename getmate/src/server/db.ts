2db4808c86aa82224f6b299b6a8e4c944e3ac790
koda0001 <wiktor.wacalw@dave-it.pl>
2026-04-03 15:25:34 +0200

    fix 2 for db

--- getmate/src/server/db.ts
+++ getmate/src/server/db.ts
@@ -1,15 +1,16 @@
 import { env } from "@/env";
 import { PrismaClient } from "../../generated/prisma";
 
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
